09379e1966adbbb929bda9fabb26576d86bd0c72
Nutthakit Anansuksakul <ing118199@gmail.com>
2022-01-12 09:52:49 +0700

    ep1

--- smartinventory/app.js
+++ smartinventory/app.js
@@ -1,11 +1,20 @@
 // importt Express
 const express = require('express')
 
+// import express flash
+const flash = require('express-flash')
+
+// import express session
+const session = require('express-session')
+
 // import EJS Layout
 const expressLayouts = require('express-ejs-layouts')
 
 // import router frontend.js
 const frontendRouter = require('./routes/frontend')
+
+// Import Router backend.js
+const backendRouter = require('./routes/backend')
 
 
 // Create express object
@@ -19,8 +28,25 @@ app.use(expressLayouts)
 app.set('layout', './layouts/frontend')
 app.set('view engine', 'ejs')
 
+// กำหนดค่าให้สามารถรับค่าจากฟอร์มได้
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// เรียกใช้งาน Express-Session
+app.use(session({
+    cookie: { maxAge: 6000 },
+    store: new session.MemoryStore,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: 'secret'
+}))
+
+// เรียกใช้ Express flash
+app.use(flash())
+
 // เรียกใช้งาน Routes
 app.use('/', frontendRouter)
+app.use('/backend', backendRouter)
 
 // Run Express Server ที่ port 5000
 app.listen(5000, () => {
--- smartinventory/routes/frontend.js
+++ smartinventory/routes/frontend.js
@@ -11,4 +11,35 @@ router.get('/about', (req,res) => {
     res.render('pages/frontend/about', {title: 'About'})
 })
 
+router.get('/login',(req, res)=>{
+    res.render(
+        'pages/frontend/login', 
+        { 
+            title: 'เข้าสู่ระบบ', 
+            layout: './layouts/authen'
+        }
+    )
+})
+
+router.get('/register',(req, res)=>{
+    res.render(
+        'pages/frontend/register', 
+        { 
+            title: 'สมัครสมาชิก', 
+            layout: './layouts/authen'
+        }
+    )
+})
+
+router.get('/forgotpassword',(req, res)=>{
+    res.render(
+        'pages/frontend/forgotpassword', 
+        { 
+            title: 'ลืมรหัสผ่าน', 
+            layout: './layouts/authen'
+        }
+    )
+})
+
+
 module.exports = router
